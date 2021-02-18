60dfdaa6f99f089cbcf1de3a35fd6acc3955a3c4
Gabriel94T <tranulovgabriel@gmail.com>
2021-02-18 15:38:26 +0000

    Added Simple Contructor

--- Hello-world/src/BigFews.java
+++ Hello-world/src/BigFews.java
@@ -1,4 +1,39 @@
 
 public class BigFews {
-
+   private String name;
+   private int numbers;
+   private boolean statement = true;
+   int something[] = {1,2,3,4,5,6};
+   
+   public BigFews() {
+	   System.out.println("Empty Constructor");
+   }
+   
+   public BigFews(String name, int numbers, boolean statement) {
+	   this.name = name;
+	   this.numbers = numbers;
+	   this.statement = statement;
+   }
+   
+   public void setName(String name) {
+	   this.name = name;
+   }
+   
+   public String getName() {
+	   return name;
+   }
+   
+   public void setNumbers(int numbers) {
+	   this.numbers = numbers;
+   }
+   
+   public int getNumbers() {
+	   return numbers;
+   }
+   public void setStatement(boolean statement) {
+	   this.statement = statement;
+   }
+   public boolean getStatement( ) {
+	   return statement;
+   }
 }
--- Hello-world/src/Main.java
+++ Hello-world/src/Main.java
@@ -2,6 +2,11 @@
 public class Main {
    public static void main(String[] args) {
 	   
+	   BigFews horse = new BigFews("Jerry", 23, false);
+	   
+	   System.out.println(horse.getName());
+	   System.out.println(horse.getNumbers());
+	   System.out.println(horse.getStatement());
 	   System.out.println("Hello There");
    }
 }
